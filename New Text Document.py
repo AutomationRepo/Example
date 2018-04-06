821b25886281d6ed23efbfdf8bd841a35236bd5d
Shweta <Shweta.gkd01@gmail.com>
2018-04-06 11:49:46 -0700

    This is just example

---
+++ New Text Document.py
@@ -0,0 +1,10 @@
+def changeList( mylist ):
+    mylist.append([1, 2, 3, 4])
+    print("Values inside function:", mylist)
+
+
+mylist = [5, 6, 7, 8]
+changeList( mylist )
+print("Values outside function:", mylist)
+g = lambda x: x**2
+print(g(5))
